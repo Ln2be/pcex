7669456009d8f563d4a5e54588ba94392a3a48f4
hacenselahy@gmail.com <hacenselahy@gmail.com>
2022-09-10 12:41:09 +0000

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const { DBDoc } = require("../lib/mongo");
+var models = require("../lib/mongo");
 
 /* GET home page. */
 router.get("/docs", async function (req, res, next) {
-  const docs = await DBDoc.find({});
+  const docs = await models.DBDoc.find({});
   res.send(docs);
 });
 
