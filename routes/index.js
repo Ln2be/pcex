1d4cee42657a2f1508cb4921ff805b7ec839e5ef
hacenselahy@gmail.com <hacenselahy@gmail.com>
2022-09-10 12:42:47 +0000

    first commit

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,12 @@ const { DBDoc } = require("../lib/mongo");
 var models = require("../lib/mongo");
 
 /* GET home page. */
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  const docs = await models.DBDoc.find({});
+  res.send("hello to pcex");
+});
+
 router.get("/docs", async function (req, res, next) {
   const docs = await models.DBDoc.find({});
   res.send(docs);
